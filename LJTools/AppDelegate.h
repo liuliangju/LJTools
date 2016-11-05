2b05d2d96dc1c9234f69e114af67dfee49ca37d6
liuliangju <liangjuliu@163.com>
2016-11-05 09:01:41 +0800

    first commit

---
+++ LJTools/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LJTools
+//
+//  Created by liuliangju on 11/5/16.
+//  Copyright © 2016 https://liuliangju.github.io. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LJTools/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LJTools
+//
+//  Created by liuliangju on 11/5/16.
+//  Copyright © 2016 https://liuliangju.github.io. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
