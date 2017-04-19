463b6410fd981552eb6d04051ec8f9fb870f1c44
liangjuliu <liangjuliu@163.com>
2017-04-19 21:22:30 +0800

    整理代码

---
+++ Pods/Headers/Private/LJMapCoordinateCorrection/LJMapCoordinateToCorrect.h
@@ -0,0 +1,1 @@
+../../../LJMapCoordinateCorrection/LJMapCoordinateCorrection/Classes/LJMapCoordinateToCorrect.h
---
+++ Pods/Target Support Files/LJMapCoordinateCorrection/LJMapCoordinateCorrection-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_LJMapCoordinateCorrection : NSObject
+@end
+@implementation PodsDummy_LJMapCoordinateCorrection
+@end
